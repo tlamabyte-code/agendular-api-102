85588574929c996a529877498c5ee450a95474a5
Eduardo ML (ieca) <instructor.emunozl@ieca.edu.mx>
2024-03-16 13:27:11 -0600

    Revert "Agendular API localhost-v1"
    This reverts commit a2e92e5f85a6fe35423b7f618fec46cb260a8d96.

--- db/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    development: { storage: "database.db", dialect: 'sqlite' }
-    // production: { storage: "database.db", dialect: 'sqlite' }
-}
--- db/models/index.js
+++
@@ -1,8 +0,0 @@
-const { Daily, DailySchema } = require('./daily.model')
-
-
-function setupModels(sequelize) {
-    Daily.init(DailySchema, Daily.config(sequelize))
-}
-
-module.exports = setupModels
